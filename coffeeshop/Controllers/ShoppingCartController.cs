fa8c225b3698b5e5e95e5b1741eaef6206b3e247
Ketnoicuuho <ductuan142563@gmail.com>
2026-06-05 10:23:55 +0700

    S5CommitAdd project files.

---
+++ coffeeshop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,57 @@
+﻿using coffeeshop.Models;
+using coffeeshop.Models.interfaces;
+using coffeeshop.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace coffeeshop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IShoppingCartRepository shoppingCartRepository;
+        private readonly IProductRepository productRepository;
+
+        public ShoppingCartController(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository)
+        {
+            this.shoppingCartRepository = shoppingCartRepository;
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var items = shoppingCartRepository.GetAllShoppingCartItems();
+            shoppingCartRepository.ShoppingCartItems = items;
+
+            ViewBag.TotalCart = shoppingCartRepository.GetShoppingCartTotal();
+
+            return View(items);
+        }
+
+        public RedirectToActionResult AddToShoppingCart(int pId)
+        {
+            var product = productRepository.GetAllProducts().FirstOrDefault(p => p.Id == pId);
+            if (product != null)
+            {
+                shoppingCartRepository.AddToCart(product);
+
+                int cartCount = shoppingCartRepository.GetAllShoppingCartItems().Sum(i => i.Qty);
+                HttpContext.Session.SetInt32("CartCount", cartCount);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int pId)
+        {
+            var product = productRepository.GetAllProducts().FirstOrDefault(p => p.Id == pId);
+            if (product != null)
+            {
+                shoppingCartRepository.RemoveFromCart(product);
+
+                int cartCount = shoppingCartRepository.GetAllShoppingCartItems().Sum(i => i.Qty);
+                HttpContext.Session.SetInt32("CartCount", cartCount);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- coffeeshop/Program.cs
+++ coffeeshop/Program.cs
@@ -1,38 +1,51 @@
 using coffeeshop.Data;
 using coffeeshop.Models.interfaces;
+using coffeeshop.Models.Interfaces;
 using coffeeshop.Models.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<CoffeeshopDbContext>(option =>
-    option.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopDbContextConnection")));
+
+builder.Services.AddDbContext<CoffeeshopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IShoppingCartRepository>(sp => ShoppingCartRepository.GetCart(sp));
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
